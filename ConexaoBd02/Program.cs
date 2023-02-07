5439c33e5048d2a55aa47a8d78babc9f87193443
Vinicius Diniz Reolon <vinicius.reolon@eldorado.org.br>
2023-02-07 11:47:42 -0300

    Commit da Aula 10 - Banco de Dados + Dapper

---
+++ ConexaoBd02/Program.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+namespace ConexaoBd02
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string? Descricao { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IncluirCategoria();
+            ListarCategoria();
+
+        }
+
+        static string conexao = @"Data Source=(localdb)\MSSQLLocalDB; 
+                                Initial Catalog=DbProdutos;
+                                Integrated Security=True";
+
+
+        static void ListarCategoria()
+        {
+            using (var conn = new SqlConnection(conexao))
+            {
+                var categorias = conn.Query<Categoria>("Select * from TBCategorias");
+                foreach (var item in categorias)
+                {
+                    Console.WriteLine("Id: " + item.Id);
+                    Console.WriteLine("Descrição: " + item.Descricao);
+                    Console.WriteLine("------------------------------------");
+                }
+
+            }
+            //static void IncluirCategoria()
+            //{
+            //    Console.Write("Informe a categoria: ");
+            //    string categoria = Console.ReadLine();
+
+            //    using (var conn = new SqlConnection(conexao)) {
+
+            //        var registros = conn.Execute("Insert into TBCategorias (Descricao) values (@Descricao)",
+            //        new { Descricao = categoria });
+            //        Console.WriteLine("Registros inseridos: " + registros);
+
+            //    }
+            //}
+
+
+
+        }
+    }
+}
---
+++ Exercicio_ConexaoBd/Program.cs
@@ -0,0 +1,110 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+namespace Exercicio_ConexaoBd
+{
+    internal class Program
+    {
+
+        // 1) Adicionar no NuGet:
+        // - System.Data.SqlClient
+        // - Dapper
+        // - Dapper.Contrib
+
+        public class Estados
+        {
+            public int Id { get; set; }
+            public string? Estado { get; set; }
+        }
+
+        public class Times
+        {
+            public int Id { get; set; }
+            public string? IdEstado { get; set; }
+            public string? Nome { get; set; }
+            public string? Cidade { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //IncluirEstado();
+            //ListarEstados();
+
+            //IncluirTimeBrasil();
+            ListarTimesBrasil();
+        }
+
+
+        // String para criar conexao com o banco
+        static string conexao = @"Data Source=(localdb)\MSSQLLocalDB; 
+                                Initial Catalog=DbTimesFutebol;
+                                Integrated Security=True";
+
+
+        // Métodos referenctes à tabela TBTimesBrasil do DbTimesFutebol
+        static void IncluirEstado()
+        {
+            Console.Write("Insira o nome do Estado que deseja adicionar ao banco de dados: ");
+            string estado = Console.ReadLine();
+
+            using (var conn = new SqlConnection(conexao))
+            {
+                var registros = conn.Execute("Insert into TBTimesBrasil (Estado) values (@Estado)",
+                new { Estado = estado });
+                Console.WriteLine("Registros inseridos: " + registros);
+            }
+        }
+
+        static void ListarEstados()
+        {
+            using (var conn = new SqlConnection(conexao))
+            {
+                var estados = conn.Query<Estados>("Select * from TBTimesBrasil");
+                foreach (var item in estados)
+                {
+                    Console.WriteLine("Id: " + item.Id);
+                    Console.WriteLine("Estado: " + item.Estado);
+                    Console.WriteLine("------------------------------------");
+                }
+            }
+        }
+
+
+        // Métodos referenctes à tabela TBTimes do DbTimesFutebol
+        static void IncluirTimeBrasil()
+        {
+            Console.Write("Insira o nome do time que deseja adicionar ao banco de dados: ");
+            string nomeTime = Console.ReadLine();
+            Console.Write("Insira o Id do estado do time que deseja adicionar ao banco de dados: ");
+            string idEstado = Console.ReadLine();
+            Console.Write("Insira a cidade do time que deseja adicionar ao banco de dados: ");
+            string cidade = Console.ReadLine();
+
+            using (var conn = new SqlConnection(conexao))
+            {
+                var registros = conn.Execute("Insert into TBTimes (IdEstado, Nome, Cidade) values (@IdEstado, @Nome, @Cidade)",
+                new { Nome = nomeTime, IdEstado = idEstado, Cidade = cidade });
+                Console.WriteLine("Registros inseridos");
+            }
+        }
+
+        static void ListarTimesBrasil()
+        {
+            using (var conn = new SqlConnection(conexao))
+            {
+                var times = conn.Query<Times>("Select * from TBTimes");
+                foreach (var item in times)
+                {
+                    Console.WriteLine("Id: " + item.Id);
+                    Console.WriteLine("Id do Estado na tabela principal: " + item.IdEstado);
+                    Console.WriteLine("Nome do time: " + item.Nome);
+                    Console.WriteLine("Cidade do time: " + item.Cidade);
+                    Console.WriteLine("------------------------------------");
+                }
+            }
+        }
+
+
+    }
+}
